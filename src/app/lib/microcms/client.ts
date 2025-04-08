86876aa96ab97c48fec856c0e0191312b7aed5f9
saki0712 <st374md@gmail.com>
2025-04-08 15:50:37 +0100

    fix: prevent caching in microcms api

--- src/app/lib/microcms/client.ts
+++ src/app/lib/microcms/client.ts
@@ -9,6 +9,9 @@ export const client = createClient({
 export const getAllBooks = async () => {
   const allBooks = await client.getList<BookType>({
     endpoint: "ebook",
+    customRequestInit: {
+      cache: "no-store",
+    },
   });
   return allBooks;
 };
@@ -17,6 +20,9 @@ export const getBookContents = async (contentId: string) => {
   const bookContents = await client.getListDetail<BookType>({
     endpoint: "ebook",
     contentId,
+    customRequestInit: {
+      cache: "no-store",
+    },
   });
   return bookContents;
 };
